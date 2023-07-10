b33552d69d8a5d775fb244cb2113d58edf1e80f9
Andre van Zuydam <andrevanzuydam@gmail.com>
2023-07-10 20:47:08 +0200

    Fix for reports , create settings.json

--- Tina4/Reporting/ReportEngine.php
+++ Tina4/Reporting/ReportEngine.php
@@ -14,6 +14,7 @@ class ReportEngine extends \Tina4\Data
      * @const string
      */
     const TINA4_REPORT_ENGINE = "." . DIRECTORY_SEPARATOR . "bin" . DIRECTORY_SEPARATOR . "reportengine";
+    const TINA4_REPORT_SETTINGS = ".".DIRECTORY_SEPARATOR . "bin" . DIRECTORY_SEPARATOR . "settings.json";
 
     /**
      * @param $reportName
@@ -26,6 +27,35 @@ class ReportEngine extends \Tina4\Data
     {
         if (file_exists($reportName)) {
             $reportName = realpath($reportName);
+        }
+
+        $settingsFile = realpath(self::TINA4_REPORT_SETTINGS);
+        if (!file_exists(self::TINA4_REPORT_SETTINGS)) {
+            $settings = json_decode('{
+  "hostName": "127.0.0.1",
+  "database": "",
+  "port" : 3050,
+  "username": "user",
+  "password": "password",
+  "driver" : "firebird-2.5"
+}');
+        } else {
+            $settings = json_decode(file_get_contents($settingsFile));
+        }
+
+        try {
+            $settings->database = $this->DBA->databaseName;
+            $settings->hostName = $this->DBA->hostName;
+            $settings->port = $this->DBA->port;
+            $settings->username = $this->DBA->username;
+            $settings->password = $this->DBA->password;
+
+            file_put_contents($settingsFile, json_encode($settings));
+
+        }
+        catch (\Exception $exception)
+        {
+            Debug::message("Cannot create settings.json for report engine", TINA4_LOG_ERROR);
         }
 
         $reportEngine = realpath(self::TINA4_REPORT_ENGINE);
